7559078ef7059536d8a13ffb14eab848ff9fead7
mattfoo5 <lematt633@gmail.com>
2023-10-16 21:23:22 -0700

    added parquet files and code

--- data_parsing/parquet_parser.py
+++ data_parsing/parquet_parser.py
@@ -4,8 +4,8 @@ import pandas as pd
 pathname = 'eg_hackathon-main/Data/player_game_stats.parquet'
 
 #dataframe = pd.read_parquet(pathname)
-dataframe = pd.read_parquet(pathname, columns=['game_urn', 'player', 'kills', 'champion'])
+dataframe = pd.read_parquet(pathname, columns=['game_urn', 'player', 'kills', 'champion']) # filer by columns
 
 
-print(dataframe)
-print(dataframe.columns)
+print(dataframe) # print resulting dataframe
+print(dataframe.columns) # print columns in dataframe
