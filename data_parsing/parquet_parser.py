c7d5f66395a6f19e02461a9659f5c8f26426080f
mattfoo5 <lematt633@gmail.com>
2023-10-16 21:02:56 -0700

    Update parquet_parser.py
    read parquet file

--- data_parsing/parquet_parser.py
+++ data_parsing/parquet_parser.py
@@ -1,3 +1,11 @@
 import pandas as pd
 
-#pathname = //Repo Path to Parquet Files
+#pathname = 'eg_hackathon-main/Data/game_summary.parquet'
+pathname = 'eg_hackathon-main/Data/player_game_stats.parquet'
+
+#dataframe = pd.read_parquet(pathname)
+dataframe = pd.read_parquet(pathname, columns=['game_urn', 'player', 'kills', 'champion'])
+
+
+print(dataframe)
+print(dataframe.columns)
