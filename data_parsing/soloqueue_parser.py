4fcbf988c6606b13cbebb6ed353e156e2e55bdbe
Isaias Andablo <andablo@usc.edu>
2023-10-19 22:26:53 -0700

    SoloQ Data + data.txt dump

--- data_parsing/soloqueue_parser.py
+++ data_parsing/soloqueue_parser.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 API_KEY = "RGAPI-ee96dbab-9b06-456a-ab15-9cd19fae58ac"
 
@@ -7,7 +8,7 @@ path_to_ids = "./lol-draft-predict_gg-c9\data_parsing\data\soloqueue_ids.txt"
 # Initialize an empty dictionary
 name_dict = {}
 puuids = {}
-
+soloq_data = {}
 # Open and read the text file
 with open(path_to_ids, 'r') as file:
     for line in file:
@@ -21,6 +22,7 @@ with open(path_to_ids, 'r') as file:
             
             # Add the name and value to the dictionary
             name_dict[name] = value
+            soloq_data[name] = {}
 
 for key, value in name_dict.items():
     # Construct the API URL with the summoner name
@@ -37,7 +39,32 @@ for key, value in name_dict.items():
         # Access the desired information from the JSON response
         puu_id = summoner_info['puuid']
         puuids[key] = puu_id
-        
+        gamecount = 0;
+        match_url = f'https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/{puu_id}/ids?start={gamecount}&count=20&api_key={API_KEY}'
+        match_response = requests.get(match_url)
+        if match_response.json():
+            #Parse MatchIDS and get information
+            for element in match_response.json():
+                game_url = f'https://asia.api.riotgames.com/lol/match/v5/matches/{element}?api_key={API_KEY}'
+                data_response = requests.get(game_url)
+                match_data = data_response.json()
+                # Find the participant data for the specified puuid
+                for participant in match_data['info']['participants']:
+                    if participant['puuid'] == puu_id:
+                        champion_id = participant['championName']
+                        win_status = participant['win']
+                        if champion_id not in soloq_data[key]:
+                            soloq_data[key][champion_id] = [0,0]
+                        if win_status:
+                            soloq_data[key][champion_id][0] += 1
+                        else: 
+                            soloq_data[key][champion_id][1] += 1
+            match_response = requests.get(match_url)
     else:
         # Handle the case when the request is not successful
         print(f"Failed to retrieve data for {value}. Status code: {response.status_code}")
+
+print(soloq_data)
+
+with open('soloq_data.txt','w') as file:
+    file.write(json.dumps(soloq_data))
